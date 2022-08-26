e4d12055151d616532324e1b5aa3fae3dacc1de6
mirenradia <mirenradia@users.noreply.github.com>
2022-08-26 14:20:40 +0000

    Update Doxygen documentation from commit 4f79f19

--- Doxygen/html/search/enumvalues_8.js
+++ Doxygen/html/search/enumvalues_8.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['odd_5fx_0',['ODD_X',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69af57cc9cdd03e56bd440ed3c48842aa95',1,'BoundaryConditions']]],
-  ['odd_5fxy_1',['ODD_XY',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69a436c974b4d628c5de86185a1b0b9f137',1,'BoundaryConditions']]],
-  ['odd_5fxyz_2',['ODD_XYZ',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69a93e5d0c5b4e6d57f9b8787b1afd421cb',1,'BoundaryConditions']]],
-  ['odd_5fxz_3',['ODD_XZ',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69af91f6b65732964aca43c6879a6fb3196',1,'BoundaryConditions']]],
-  ['odd_5fy_4',['ODD_Y',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69aedb3ac0f316ff169f26648ef01be0c96',1,'BoundaryConditions']]],
-  ['odd_5fyz_5',['ODD_YZ',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69ac453fd7cd7971f7fcc30b1e725570515',1,'BoundaryConditions']]],
-  ['odd_5fz_6',['ODD_Z',['../class_boundary_conditions.html#a328768c7aaea35ede2e2f2a04ba60b69a6ae42589589b03f040a7c523e97e724d',1,'BoundaryConditions']]],
+  ['odd_5fx_0',['ODD_X',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46af57cc9cdd03e56bd440ed3c48842aa95',1,'BoundaryConditions']]],
+  ['odd_5fxy_1',['ODD_XY',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46a436c974b4d628c5de86185a1b0b9f137',1,'BoundaryConditions']]],
+  ['odd_5fxyz_2',['ODD_XYZ',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46a93e5d0c5b4e6d57f9b8787b1afd421cb',1,'BoundaryConditions']]],
+  ['odd_5fxz_3',['ODD_XZ',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46af91f6b65732964aca43c6879a6fb3196',1,'BoundaryConditions']]],
+  ['odd_5fy_4',['ODD_Y',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46aedb3ac0f316ff169f26648ef01be0c96',1,'BoundaryConditions']]],
+  ['odd_5fyz_5',['ODD_YZ',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46ac453fd7cd7971f7fcc30b1e725570515',1,'BoundaryConditions']]],
+  ['odd_5fz_6',['ODD_Z',['../class_boundary_conditions.html#a25cc34022bff47dc98a4d1f5e86f7c46a6ae42589589b03f040a7c523e97e724d',1,'BoundaryConditions']]],
   ['one_7',['ONE',['../_binary_b_h_8hpp.html#aa9d3912d28541d5cac1fe9d89b8dc230a7a725f13af144bdef532d0389ba75e0d',1,'BinaryBH.hpp']]]
 ];
